cd77cdc5086684954c608329d4dc171f93ba5790
RC1844 <1844766234@qq.com>
2019-08-01 13:50:47 +0800

    添加项目文件。

---
+++ Main.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace AppName
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Main
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace AppName
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog
+            {
+                Title = "选择文件夹",
+                InitialDirectory = @"",   //@是取消转义字符的意思
+                RestoreDirectory = false,
+                IsFolderPicker = true
+            };
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                textBox1.Text = dialog.FileName;
+            }
+        }
+
+        private bool NotNull(ref string Target, string str)
+        {
+            bool b = true;
+            if (str != null && !str.Equals(""))
+            {
+                str=System.Text.RegularExpressions.Regex.Replace(str, @"\s{1,}", "");
+                if (str != null && !str.Equals(""))
+                {
+                    Target = str;
+                    b = false;
+                }
+            }
+            return b;
+        }
+        private void RenameFile(string DirPath)
+        {
+            if (Directory.Exists(DirPath))
+            {
+                foreach (string FilePath in Directory.GetFiles(DirPath, "*.exe"))
+                {
+                    if (File.Exists(FilePath))
+                    {
+                        Console.WriteLine(FilePath);
+                        FileVersionInfo FileInfo = FileVersionInfo.GetVersionInfo(FilePath);
+                        string Version = string.Format("{0}.{1}.{2}.{3}", FileInfo.FileMajorPart, FileInfo.FileMinorPart, FileInfo.FileBuildPart, FileInfo.FilePrivatePart);
+                        string FileName = Path.GetFileNameWithoutExtension(FilePath);
+                        if(NotNull(ref FileName, FileInfo.ProductName))
+                        {
+                            _ = NotNull(ref FileName, FileInfo.InternalName);
+                        }
+
+                        if (Version.Equals("0.0.0.0"))
+                        {
+                            Version = "";
+                            if(NotNull(ref Version, FileInfo.FileVersion))
+                            {
+                                _ = NotNull(ref Version, FileInfo.ProductVersion);
+                            }
+                        }
+                        if (!Version.Equals(""))
+                        {
+                            FileName += "_" + Version;
+                        }
+                        FileName += ".exe";
+                        Console.WriteLine(FileName);
+                        FileName = textBox1.Text + "\\" + FileName;
+                        File.Move(FilePath, FileName);
+                    }
+                }
+            }
+        }
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            RenameFile(textBox1.Text);
+        }
+    }
+}
